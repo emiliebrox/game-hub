d645566b85704af4b55ab2d70ba2aa13580385da
Emilie Brox <emiliebrox@gmail.com>
2023-10-16 00:14:16 +0200

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -20,7 +20,10 @@ const useGames = (
   gameQuery: GameQuery
 ) => useData<Game>('/games', { 
   params: {
-    genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id }}, 
+    genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id,
+    ordering: gameQuery.sortOrder
+  },
+}, 
     [gameQuery]);
 
 export default useGames; 
